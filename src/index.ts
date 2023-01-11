1083b514ae56053d4e7df24d604c1fcb82d0c30b
Dzianis Petrykau <desik1603@gmail.com>
2023-01-11 17:51:25 +0300

    feat: initial req for all users was implemented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-import path from 'path'
-console.log('hello', path.dirname('./index.ts'));
+import http from 'node:http';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const PORT = process.env.PORT;
 
+const usersList = [];
 
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  switch (req.url) {
+    case '/api/users':
+      res.writeHead(200);
+      res.end(JSON.stringify(usersList));
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'Resource not found' }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

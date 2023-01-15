4147bd932134427c08a80f9519493b66fee648c8
Dzianis Petrykau <desik1603@gmail.com>
2023-01-15 17:09:22 +0300

    feat: put, delete requests were implemented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import http, { IncomingMessage, ServerResponse } from 'node:http';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4, validate } from 'uuid';
 import dotenv from 'dotenv';
 import { userType } from './types.js';
 import {
@@ -26,29 +26,49 @@ const server = http.createServer(
     const [, , userId, perspectiveInIT] = req.url.split('/').filter(Boolean);
     try {
       if (req.url.startsWith('/api/users') && !perspectiveInIT) {
-        switch (req.method) {
-          case 'GET':
-            responseBody = userId
-              ? getUserController(userId, usersList)
-              : usersList;
-            break;
-          case 'POST':
-            // eslint-disable-next-line no-case-declarations
+        if (req.method === 'GET') {
+          responseBody = userId
+            ? getUserController(userId, usersList)
+            : usersList;
+        } else if (req.method === 'POST') {
+          const requestBody: userType = JSON.parse(await getRequestBody(req));
+          if (isValidUser(requestBody)) {
+            const { username, age, hobbies } = requestBody;
+            const newUser = { id: uuidv4(), username, age, hobbies };
+            usersList.push(newUser);
+            responseBody = newUser;
+          } else {
+            throw new Error(INCONSISTENT_DATA);
+          }
+        } else if (req.method === 'PUT') {
+          if (!validate(userId)) {
+            throw new Error(INVALID_USER_ID);
+          }
+          const userIdx = usersList.findIndex((user) => user.id === userId);
+          if (userIdx !== -1) {
             const requestBody: userType = JSON.parse(await getRequestBody(req));
-            if (isValidUser(requestBody)) {
-              const { username, age, hobbies } = requestBody;
-              const newUser = { id: uuidv4(), username, age, hobbies };
-              usersList.push(newUser);
-              responseBody = newUser;
-            } else {
-              throw new Error(INCONSISTENT_DATA);
-            }
-            break;
-            case 'PUT': 
-            
-            break;
-          default:
-            break;
+            const { username, age, hobbies } = requestBody;
+            usersList.splice(userIdx, 1, {
+              id: userId,
+              username,
+              age,
+              hobbies,
+            });
+            responseBody = { id: userId, username, age, hobbies };
+          } else {
+            throw new Error(USER_NOT_FOUND);
+          }
+        } else if (req.method === 'DELETE') {
+          if (!validate(userId)) {
+            throw new Error(INVALID_USER_ID);
+          }
+          const userIdx = usersList.findIndex((user) => user.id === userId);
+          if (userIdx !== -1) {
+            usersList.splice(userIdx, 1);
+            statusCode = 204;
+          } else {
+            throw new Error(USER_NOT_FOUND);
+          }
         }
       } else {
         res.writeHead(404);
@@ -57,7 +77,10 @@ const server = http.createServer(
       res.writeHead(statusCode);
       res.end(JSON.stringify(responseBody));
     } catch (error) {
-      if (error.message === INVALID_USER_ID || error.message === INCONSISTENT_DATA) {
+      if (
+        error.message === INVALID_USER_ID ||
+        error.message === INCONSISTENT_DATA
+      ) {
         statusCode = 400;
       } else if (error.message === USER_NOT_FOUND) {
         statusCode = 404;
@@ -65,7 +88,7 @@ const server = http.createServer(
         statusCode = 500;
       }
       res.writeHead(statusCode);
-      res.end(JSON.stringify({ code: statusCode, message: error.message }));
+      res.end(JSON.stringify({ code: statusCode, message: statusCode !== 500? error.message: SERVER_ERROR }));
     }
   }
 );
--- src/requestController/postUserController.ts
+++
@@ -1,3 +0,0 @@
-import { INCONSISTENT_DATA } from '../constants.js';
-import { userType } from '../types.js';
-import { isValidUser } from '../utils.js';
